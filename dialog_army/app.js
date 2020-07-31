c1c64362dbf5e75bb251766a57aa2eea919f26bc
v1rtl <pilll.PL22@gmail.com>
2020-08-01 00:47:15 +0300

    aaaa

--- dialog_army/app.js
+++ dialog_army/app.js
@@ -1,41 +1,36 @@
-let main = document.querySelector("main");
-let index = Math.ceil(Math.random() * 12);
-console.log(index);
+let main = document.querySelector('main')
+let index = Math.ceil(Math.random() * 12)
+console.log(index)
 for (let i = 0; i < 12; i++) {
   if (index != i) {
-    let div = document.createElement("div");
-    let btn = document.createElement("button");
-    btn.innerText = "X";
-    div.innerText = i;
+    let div = document.createElement('div')
+    let btn = document.createElement('button')
+    btn.innerText = 'X'
+    div.innerText = `${i}`
     btn.onclick = (e) => {
-      e.currentTarget.parentElement.remove();
-    };
-    div.appendChild(btn);
-    main.appendChild(div);
+      e.currentTarget.parentElement.remove()
+    }
+    div.appendChild(btn)
+    main.appendChild(div)
   } else {
-    let div = document.createElement("div");
-    div.innerText = i;
-    let btn = document.createElement("button");
-    btn.innerText = "X";
-    div.appendChild(btn);
-    main.appendChild(div);
+    let div = document.createElement('div')
+    div.innerText = `${i}`
+    let btn = document.createElement('button')
+    btn.onclick = () => {
+      if (Array.from(main.children).length === 1) {
+        let oldBtn = document.querySelector('main div button')
+        oldBtn.remove()
+        let btn = document.createElement('button')
+        btn.id = 'join'
+        btn.innerText = 'Принять участие'
+        btn.onclick = () => visit('cube_faces')
+        const lastDiv = document.querySelector('main div')
+
+        lastDiv.appendChild(btn)
+      }
+    }
+    btn.innerText = 'X'
+    div.appendChild(btn)
+    main.appendChild(div)
   }
 }
-console.log(main);
-
-/*const main = document.querySelector('main')
-
-  const btn = document.createElement('button')
-
-  btn.innerText = 'X'
-
-  btn.class = 'close'
-
-  btn.onclick = () => {
-    visit('scroll_to_top')
-    location.reload()
-  }
-
-  main.scrollTo({ top: 0 })
-
-  main.appendChild(btn)*/
