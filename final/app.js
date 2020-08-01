8443d339e92593d7dcaafce51bfd140d3dc0e858
v1rtl <pilll.PL22@gmail.com>
2020-08-02 01:07:01 +0300

    aa

---
+++ final/app.js
@@ -0,0 +1,9 @@
+const cont = document.getElementById('code')
+
+for (let i = 0; i < 1000; i++) {
+  const img = document.createElement('img')
+
+  img.src = '/final/oleg.png'
+
+  cont.appendChild(img)
+}
